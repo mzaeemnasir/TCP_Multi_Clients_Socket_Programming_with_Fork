74a68cfc5bc32bb9d1602affcc6cde113b11d347
mzaeemnasir <m.zaeem.nasir@gmail.com>
2021-12-26 21:30:53 +0500

    first commit

---
+++ Client.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+using namespace std;
+int main()
+{
+    char Buffer[1024] = {0};
+    int Port = 3000;
+    // Creating Socket
+    // AF_INET - IPv4
+    // SOCK_STREAM - TCP
+    // Protocol Value = 0
+    int Socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (Socket == 0)
+    {
+        cout << "Socket Creation Failed" << endl;
+        exit;
+    }
+
+    // Connecting to the server
+    struct sockaddr_in Server_Addr;
+    Server_Addr.sin_family = AF_INET;
+    Server_Addr.sin_port = htons(Port);
+    // inet_pton(AF_INET, "127.0.0.1", &Server_Addr.sin_addr);
+    int Connect = connect(Socket, (struct sockaddr *)&Server_Addr, sizeof(Server_Addr));
+    if (Connect < 0)
+    {
+        cout << "Connection Failed" << endl;
+        exit;
+    }
+    else
+    {
+        cout << "Connection Successful" << endl;
+    }
+    // receiving message from the server
+
+    while (read(Socket, Buffer, sizeof(Buffer)) > 0)
+    {
+        cout << Buffer << endl;
+        memset(Buffer, 0, sizeof(Buffer));
+    }
+
+    close(Socket);
+    return 0;
+}
---
+++ Server.cpp
@@ -0,0 +1,116 @@
+// TCP Server using C Libraries
+
+// Name: Muhammad Zaeem Nasir
+// Roll: 19F-0355
+// Section: B
+
+// Flow Chart
+
+// 1. Create a socket  & Attach it to a port
+// 2. Bind the socket to a port
+// 3. Listen for connections
+// 4. Accept the connection
+// 5. Receive data
+// 6. Send data
+
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <fstream>
+#include <arpa/inet.h>
+using namespace std;
+
+int main()
+{
+    // Creating Socket
+    // AF_INET - IPv4
+    // SOCK_STREAM - TCP
+    // 0 - Protocol Value
+    int Socket = socket(AF_INET, SOCK_STREAM, 0); // Here AF_INET is the address family, which is IPv4
+    int Set_Sock_opt;
+    int Port = 3000;
+    int opt = 1;
+    char Buffer[1024] = {0};
+    if (Socket == 0)
+    {
+        cout << "Socket Creation Failed" << endl;
+        exit;
+    }
+    // Now our Socket is created, Attatching Socket to the port
+    // SOL_SOCKET - Socket Level
+    // SO_REUSEADDR - Socket Option
+    Set_Sock_opt = setsockopt(Socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+
+    // Attaching Socket to the port
+    if (Set_Sock_opt < 0)
+    {
+        cout << "Socket Attachment Failed" << endl;
+        exit;
+    }
+
+    // Binding Socket to the port
+    struct sockaddr_in Server_Addr;
+    // sockaddr_in is a structure that contains an internet address
+    Server_Addr.sin_family = AF_INET;         // Address Family - Ip Address Type - IPv4
+    Server_Addr.sin_port = htons(Port);       // Port Number
+    Server_Addr.sin_addr.s_addr = INADDR_ANY; // INADDR_ANY - Any IP Address
+
+    // Binding Socket to the port
+    int Bind = bind(Socket, (struct sockaddr *)&Server_Addr, sizeof(Server_Addr));
+
+    // if the Binind is not successful
+    if (Bind < 0)
+    {
+        cout << "Socket Binding Failed" << endl;
+        exit;
+    }
+
+    // Listening for connections
+    int Listen = listen(Socket, 3);
+    if (Listen < 0)
+    {
+        cout << "Socket Listening Failed" << endl;
+        exit;
+    }
+    else
+    {
+        cout << "Server is Listening" << endl;
+    }
+    // Accepting the connection
+    int addrlen = sizeof(Server_Addr);
+    int Client_Socket;
+
+    // using while loop for accepting Multiple Clients
+    while (1)
+    {
+        Client_Socket = accept(Socket, (struct sockaddr *)&Server_Addr, (socklen_t *)&addrlen);
+        if (Client_Socket < 0)
+        {
+            exit(1);
+        }
+        cout << "Connected to Client " << endl;
+        if (fork() == 0)
+        {
+            close(Socket);
+            while (1)
+            {
+                // Sending Message to the Client
+                //  Sending File Data
+                ifstream inputfile;
+                inputfile.open("textfile.txt");
+                char line[1024];
+                while (inputfile.getline(line, sizeof(line)))
+                {
+                    send(Client_Socket, line, sizeof(line), 0);
+                }
+                inputfile.close();
+                break;
+            }
+        }
+    }
+    close(Client_Socket);
+    return 0;
+}
